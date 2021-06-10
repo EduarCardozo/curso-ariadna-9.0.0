a6bd053b7528049f96f743f34969ca94bb5098f4
EduarCardozo <cardozoguerreroeduar@gmail.com>
2021-06-10 17:37:11 -0500

    Custom activo_crear
    Module Custom activo_crear

---
+++ web/sites/default/files/php/twig/60c13e1ed9d89_views-view-field.html.twi_3tZSqJFCI3pWXTA5zDYLTIQJA/k1X8mHenG8CPAONXfqgnRNlrejK52h5hSoesYuhf-Bs.php
@@ -0,0 +1,84 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/views/templates/views-view-field.html.twig */
+class __TwigTemplate_ae90cedd5d2a9e2e205b338b0924cdf19673a74d32f6b0f7c8c8390dbd44b5a9 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 23);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 23
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["output"] ?? null), 23, $this->source), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/views/templates/views-view-field.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 23,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/views/templates/views-view-field.html.twig", "D:\\wamp64\\www\\ariadna\\curso\\web\\core\\modules\\views\\templates\\views-view-field.html.twig");
+    }
+}
---
+++ web/sites/default/files/php/twig/60c13e1ed9d89_we-megamenu-ul.html.twig_fUtwuCrTNcUQJqgFUmK5QA0jI/OWCgdm46bpFzAeCY9YEo3RRwkNZonQ-xT_2o8GW0YOE.php
@@ -0,0 +1,91 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/we_megamenu/templates/we-megamenu-ul.html.twig */
+class __TwigTemplate_7f86e5238626ac0fca65e0dbe5b9da595e6c38c15a0ee553867a8f8073cec9ca extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 1);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<ul ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 1, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 2, $this->source), "html", null, true);
+        echo "
+</ul>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/we_megamenu/templates/we-megamenu-ul.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  67 => 2,  62 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/we_megamenu/templates/we-megamenu-ul.html.twig", "D:\\wamp64\\www\\ariadna\\curso\\web\\modules\\contrib\\we_megamenu\\templates\\we-megamenu-ul.html.twig");
+    }
+}
